daeef0ec9f8bfaccc81fe07ce2a01e806fd63acd
Arvin Behbahani <arvinbehbahani562@gmail.com>
2025-10-24 23:39:38 +0330

    finished my_temp site

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
-from home.views import home_view
+from home.views import home_view, about_view, contact_view
+
+app_name = "home"
 
 urlpatterns = [
-    path("", home_view, name="index")
+    path("", home_view, name="index"),
+    path("about/", about_view, name="about"),
+    path("contact/", contact_view, name="contact")
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-app_name = "home"
+
+my_info = {"name": "Arvin", "last_name": "Behbahani",
+           "WF": "django", "phone": "(+98) 933-881-3757",
+           "loc": "Iran, Tehran", "email": "arvinbehbahani@gmail.com"}
 
 
 def home_view(request):
-    return render(request, "resume/index.html")
+
+    return render(request, "resume/index.html", my_info)
+
+
+def about_view(request):
+    return render(request, "resume/about.html", my_info)
+
+
+def contact_view(request):
+    return render(request, "resume/contact.html", my_info)
