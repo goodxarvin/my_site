e2be591265938a840b1ca20f5d62b5f88bdb7e87
Arvin Behbahani <arvinbehbahani562@gmail.com>
2025-10-25 06:15:44 +0330

    created migrations files (makemigrations and migrate) also added these files to  (shell and admin)

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    # image
+    # author
+    # tag
+    # category
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    status = models.BooleanField(default=False)
+    counted_views = models.IntegerField(default=0)
+    published_date = models.DateTimeField(null=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+
+# 
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Submitted(models.Model):
+    name = models.CharField(max_length=20)
+    subject = models.CharField(max_length=60)
+    email = models.EmailField()
+    messsage = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
