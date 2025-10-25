ff661050347ef8006fb98027ac1bac09a4b45881
Arvin Behbahani <arvinbehbahani562@gmail.com>
2025-10-25 06:31:24 +0330

    override the __str_() method for the Post table

--- blog/models.py
+++ blog/models.py
@@ -14,5 +14,8 @@ class Post(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.id}, {self.title}"
 
-# 
+
+#
