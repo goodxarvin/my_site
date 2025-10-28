fff322a6b85b6658bdc49eec8d8003fe846b79ce
Arvin Behbahani <arvinbehbahani562@gmail.com>
2025-10-28 04:08:32 +0330

    added go to prev/next for blogs

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
-from django.db.models import F
+from django.db.models import F, Max
 from django.utils import timezone
+
+
+def get_prev_next(post_id: int):
+    prev_post = None
+    next_post = None
+    max_id = Post.objects.aggregate(Max("id"))["id__max"]
+    for prev in range(post_id-1, 0, -1):
+        if Post.objects.filter(id=prev, status=1).exists():
+            prev_post = get_object_or_404(Post, pk=prev, status=1)
+            break
+    for the_next in range(post_id+1, max_id+1):
+        if Post.objects.filter(id=the_next, status=1).exists():
+            next_post = get_object_or_404(Post, pk=the_next, status=1)
+            break
+
+    return prev_post, next_post
 
 
 def single_view(request, slug, post_id):
     post = get_object_or_404(Post, pk=post_id, status=1)
+    prev_p, next_p = get_prev_next(post_id)
     # posts = Post.objects.filter(status=1)
     # post = get_object_or_404(posts, pk=post_id) ==> second way for the top code
+
     paragraph = post.content.split("\n")
-    context = {"post": post, "paragraph": paragraph}
+    context = {"post": post, "paragraph": paragraph,
+               "next": next_p, "prev": prev_p}
     return render(request, "travelista/blog/blog-single.html", context)
 
 
