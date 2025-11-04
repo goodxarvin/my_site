d9a29d262ab8589b74001162b49887becce465d4
Arvin Behbahani <arvinbehbahani562@gmail.com>
2025-11-04 04:19:13 +0330

    finished the login page template and functionality

--- user_management/views.py
+++ user_management/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 
 def login_view(request):
-    return render(request, "travelista/user_management/login.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect("/")
+
+        else:
+            messages.add_message(request, messages.ERROR,
+                                 "wrong username or password")
+    context = {"request": request}
+    return render(request, "travelista/user_management/login.html", context)
 
 
 # def logout_view(request):
