9dffc61f42008421a25c17bd2072af740a375810
Arvin Behbahani <arvinbehbahani562@gmail.com>
2025-11-04 23:56:24 +0330

    added signup page and lots of bug fixing

--- user_management/views.py
+++ user_management/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm  # , UserCreationForm
 from django.contrib import messages
+from user_management.forms import UserForm
 
 
 def login_view(request):
@@ -24,10 +25,11 @@ def login_view(request):
                 messages.add_message(request, messages.ERROR,
                                      "wrong username or password")
     else:
+        messages.add_message(
+            request, messages.INFO, "you're already logged in please logout to proceed the following task")
         return redirect("/")
 
-    context = {"request": request}
-    return render(request, "travelista/user_management/login.html", context)
+    return render(request, "travelista/user_management/login.html")
 
 
 @login_required()
@@ -37,4 +39,20 @@ def logout_view(request):
 
 
 def signup_view(request):
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            form = UserForm(request.POST)
+            if form.is_valid():
+                messages.add_message(
+                    request, messages.SUCCESS, "your account created successfully")
+                form.save()
+                return redirect("/user-management/login")
+            else:
+                messages.add_message(
+                    request, messages.ERROR, "your account didn't created")
+    else:
+        messages.add_message(
+            request, messages.INFO, "you're already logged in please logout to proceed the following task")
+        return redirect("/")
+
     return render(request, "travelista/user_management/signup.html")
