6b630f89e83d3c5843211bf256bb2e0084ff2705
Arvin Behbahani <arvinbehbahani562@gmail.com>
2025-11-04 04:48:34 +0330

    added a better way to authenticate users 'AuthenticationForm'

--- user_management/views.py
+++ user_management/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 
 
 def login_view(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user:
-            login(request, user)
-            return redirect("/")
+    if request.method == "POST" and not request.user.is_authenticated:
+        form = AuthenticationForm(
+            request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect("/")
 
-        else:
-            messages.add_message(request, messages.ERROR,
-                                 "wrong username or password")
+            else:
+                messages.add_message(request, messages.ERROR,
+                                     "wrong username or password")
+    else:
+        return redirect("/")
+
     context = {"request": request}
     return render(request, "travelista/user_management/login.html", context)
 
