dec95c69ef2b77d1d9e93da7b4b4fa3617fbb622
Arvin Behbahani <arvinbehbahani562@gmail.com>
2025-11-16 20:09:04 +0330

    optimised the sites security

--- my_site/setting/prod.py
+++ my_site/setting/prod.py
@@ -29,7 +29,7 @@ CSRF_COOKIE_SECURE = True
 
 SESSION_COOKIE_SECURE = True
 
-# postgresql 
+# postgresql
 
 DATABASES = {
     'default': {
@@ -41,3 +41,25 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+
+# site protection
+
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+# X-Content-Type-Options
+SECURE_CONTENT_TYPE_NOSNIFF = True
+# Strict-Transport-Security
+SECURE_HSTS_SECONDS = 15768000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+# that requests over HTTP are redirected to HTTPS. aslo can config in webserver
+SECURE_SSL_REDIRECT = True
+
+# for more security
+CSRF_COOKIE_SECURE = True
+CSRF_USE_SESSIONS = True
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = 'Strict'
